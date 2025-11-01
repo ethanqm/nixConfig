01ad65c47bbf3d0691e0944a80d14429f9634698
ethanqm <102937457+ethanqm@users.noreply.github.com>
2025-11-01 17:36:06 +1100

    fixing oauth

--- home.nix
+++ home.nix
@@ -93,6 +93,11 @@
     userEmail = "102937457+ethanqm@users.noreply.github.com";
     extraConfig = {
       init.defaultBranch = "main";
+
+      credential = {
+        helper = [ "cache --timeout 21600"  "oauth" ];
+      };
+
       diff.tool = "vimdiff";
       difftool.prompt = false;
     };
