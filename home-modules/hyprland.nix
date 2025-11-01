f7d1fb581218f730d28da61992246f1b36d155f6
ethanqm <q.ethan.moore@gmail.com>
2025-11-01 13:32:57 +1100

    workspace hotkeys hyprland

--- home-modules/hyprland.nix
+++ home-modules/hyprland.nix
@@ -110,6 +110,17 @@ in
         "$mod SHIFT, 8, movetoworkspace, 8"
         "$mod SHIFT, 9, movetoworkspace, 9"
         "$mod SHIFT, 0, movetoworkspace, 10"
+        # Switch to workspace
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
         # Example special workspace (scratchpad)
         #"$mod, S, togglespecialworkspace, magic"
         #"$mod SHIFT, S, movetoworkspace, special:magic"
