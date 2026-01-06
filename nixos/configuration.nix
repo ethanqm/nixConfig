55da02a39fa0f7add944e7315a4fdfb63eb5bac6
ethanqm <102937457+ethanqm@users.noreply.github.com>
2026-01-06 16:43:50 +1100

    trying to fix mic vol changing at work

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,16 +88,32 @@
         services.pulseaudio.enable = false;
         security.rtkit.enable = true;
         services.pipewire = {
-        enable = true;
-        alsa.enable = true;
-        alsa.support32Bit = true;
-        pulse.enable = true;
-# If you want to use JACK applications, uncomment this
-#jack.enable = true;
+          enable = true;
+          alsa.enable = true;
+          alsa.support32Bit = true;
+          pulse.enable = true;
+  # If you want to use JACK applications, uncomment this
+  #jack.enable = true;
+          extraConfig = {
+            # chromium apps reset mic vol to ~25% without permission
+            pipewire-pulse."99-chromium-mic-vol-fix" = {
+              "pulse.rules" = [
+                {
+                  matches = [ 
+                    { "application.name" = "~Microsoft.*"; }
+                    { "application.name" = "~Chromium.*"; }
+                    { "application.process.binary" = "Discord"; }
+                  ];
+                  actions = { quirks = [ "block-source-volume" ]; };
+                }
+              ];
+            };
+          };
 
-# use the example session manager (no others are packaged yet so this is enabled by default,
-# no need to redefine it in your config for now)
-#media-session.enable = true;
+  
+  # use the example session manager (no others are packaged yet so this is enabled by default,
+  # no need to redefine it in your config for now)
+  #media-session.enable = true;
         };
 
 # Enable touchpad support (enabled default in most desktopManager).
